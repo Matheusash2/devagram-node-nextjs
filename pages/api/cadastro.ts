f255a7aa55348adb265da5d0082deee6e97b3d7f
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2023-04-20 08:42:15 -0300

    alteracao no campo email(regex)

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -17,9 +17,8 @@ const handler = nc()
                 return res.status(400).json({erro: 'Nome invalido'});
             }
     
-            if(!usuario.email || usuario.email.length < 5
-                || !usuario.email.includes('@') 
-                || !usuario.email.includes('.')){
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+(\.[^\s@]+)?$/;
+            if(!emailRegex.test(usuario.email)){
                 return res.status(400).json({erro: 'Email invalido'});
             }
     
